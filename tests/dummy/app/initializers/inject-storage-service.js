27093c337905bfadd099592736cf7c5c0d8d8e41
Kenneth Buck <kenneth@nestedsphere.com>
2015-09-13 20:53:08 -0400

    added example injection initializer

---
+++ tests/dummy/app/initializers/inject-storage-service.js
@@ -0,0 +1,12 @@
+export function initialize(registry, application) {
+	application.inject('service:session', 'storageService', 'service:storage');
+	application.inject('adapter', 'storageService', 'service:storage');
+	application.inject('controller', 'storageService', 'service:storage');
+	application.inject('model', 'storageService', 'service:storage');
+	application.inject('route', 'storageService', 'service:storage');
+}
+
+export default {
+	name: 'inject-storage-service',
+	initialize: initialize
+};
