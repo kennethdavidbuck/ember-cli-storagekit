dc00e0164b1482cf12b4ca679c979772d3017eb3
Kenneth Buck <kenneth@nestedsphere.com>
2015-09-15 19:27:47 -0400

    setup boilerplate unit test for storage support utility

---
+++ tests/utilities/storage-support-test.js
@@ -0,0 +1,12 @@
+import StorageSupportUtility from '../../../utilities/storage-support';
+import { module, test } from 'ember-qunit';
+
+module('Unit | Utility | Storage Support', {
+  // Specify the other units that are required for this test.
+  // needs: ['service:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  assert.ok(StorageSupportUtility);
+});
