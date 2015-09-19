f80f84daa0d7981b89294856e4390dcca4424e08
Kenneth Buck <kenneth@nestedsphere.com>
2015-09-19 16:49:38 -0400

    docs

--- addon/serializers/json.js
+++ addon/serializers/json.js
@@ -4,12 +4,32 @@ import Ember from 'ember';
 
 const {isPresent} = Ember;
 
+/**
+ * @module ember-cli-storagekit
+ * @submodule serializers
+ */
+
+/**
+ * @class Json
+ * @namespace EmberCliStoragekit
+ * @extends Ember.Object
+ * @public
+ */
+
 export default Ember.Object.extend({
 
+  /**
+   * @method serialize
+   * @public
+   */
   serialize(value) {
     return JSON.stringify(value);
   },
 
+  /**
+   * @method deserialize
+   * @public
+   */
   deserialize(value) {
     if (isPresent(value)) {
       value = JSON.parse(value);
