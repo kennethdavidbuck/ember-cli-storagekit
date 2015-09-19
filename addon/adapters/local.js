3657f5d069427482c77f5f6b86824278f88cad7b
Kenneth Buck <kenneth@nestedsphere.com>
2015-09-19 16:46:35 -0400

    docs

--- addon/adapters/local.js
+++ addon/adapters/local.js
@@ -3,28 +3,67 @@ import Ember from 'ember';
 
 /*global window*/
 
+/**
+ * @module ember-cli-storagekit
+ * @submodule adapters
+ */
+
+/**
+ * @class Local
+ * @namespace EmberCliStoragekit
+ * @extends Ember.Object
+ * @uses EmberCliStoragekit/Mixins/BuildNamespace
+ * @public
+ */
+
 export default Ember.Object.extend(BuildNamespaceMixin, {
 
+  /**
+   * @property {*} storage
+   */
   storage: window.localStorage,
 
+  /**
+   * @property {*} serializer
+   */
   serializer: null,
 
+  /**
+   * @method setItem
+   * @public
+   */
   setItem(key, value) {
     this.get('storage').setItem(this.buildNamespace(key), this.get('serializer').serialize(value));
   },
 
+  /**
+   * @method getItem
+   * @public
+   */
   getItem(key) {
     return this.get('serializer').deserialize(this.get('storage').getItem(this.buildNamespace(key)));
   },
 
+  /**
+   * @method removeItem
+   * @public
+   */
   removeItem(key){
     this.get('storage').removeItem(this.buildNamespace(key));
   },
 
+  /**
+   * @method clear
+   * @public
+   */
   clear() {
     this.get('storage').clear();
   },
 
+  /**
+   * @method length
+   * @public
+   */
   length() {
     return this.get('storage.length');
   }
--- addon/adapters/object.js
+++ addon/adapters/object.js
@@ -1,28 +1,67 @@
 import BuildNamespaceMixin from '../mixins/build-namespace';
 import Ember from 'ember';
 
+/**
+ * @module ember-cli-storagekit
+ * @submodule adapters
+ */
+
+/**
+ * @class Object
+ * @namespace EmberCliStoragekit
+ * @extends Ember.Object
+ * @uses EmberCliStoragekit/Mixins/BuildNamespace
+ * @public
+ */
+
 export default Ember.Object.extend(BuildNamespaceMixin, {
 
+  /**
+   * @property {Ember.Map} storage
+   */
   storage: Ember.Map.create(),
 
+  /**
+   * @property {*} serializer
+   */
   serializer: null,
 
+  /**
+   * @method setItem
+   * @public
+   */
   setItem(key, value) {
     this.get('storage').set(this.buildNamespace(key), this.get('serializer').serialize(value));
   },
 
+  /**
+   * @method getItem
+   * @public
+   */
   getItem(key) {
     return this.get('serializer').deserialize(this.get('storage').get(this.buildNamespace(key)));
   },
 
+  /**
+   * @method removeItem
+   * @public
+   */
   removeItem(key){
     this.get('storage').delete(this.buildNamespace(key));
   },
 
+  /**
+   * @method clear
+   * @public
+   */
   clear() {
     this.get('storage').clear();
   },
 
+  /**
+   * @method length
+   * @public
+   */
   length() {
     return this.get('storage.size');
   }
