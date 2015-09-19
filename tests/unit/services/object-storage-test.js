03bbc6b3c7f5eea332ccb33dabc88939e5fe1508
Kenneth Buck <kenneth@nestedsphere.com>
2015-09-19 16:12:11 -0400

    removed superfluous comments

--- tests/unit/services/object-storage-test.js
+++ tests/unit/services/object-storage-test.js
@@ -2,7 +2,6 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('storagekit/service:object-storage', 'Unit | Service | object storage', {});
 
-// Replace this with your real tests.
 test('it exists', function (assert) {
   var service = this.subject();
   assert.ok(service);
--- tests/unit/services/storage-test.js
+++ tests/unit/services/storage-test.js
@@ -2,7 +2,6 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('storagekit/service:storage', 'Unit | Service | Storage', {});
 
-// Replace this with your real tests.
 test('it exists', function (assert) {
   var service = this.subject();
   assert.ok(service);
