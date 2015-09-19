bc44ed3333d3d8d5793225854ab3b150c7652800
Kenneth Buck <kenneth@nestedsphere.com>
2015-09-19 14:05:50 -0400

    pod paths in dummy app inject-storage-service initializer

--- tests/dummy/app/initializers/inject-storage-service.js
+++ tests/dummy/app/initializers/inject-storage-service.js
@@ -1,9 +1,9 @@
 export function initialize(registry, application) {
-  application.inject('service:session', 'storageService', 'service:storage');
-  application.inject('adapter', 'storageService', 'service:storage');
-  application.inject('controller', 'storageService', 'service:storage');
-  application.inject('model', 'storageService', 'service:storage');
-  application.inject('route', 'storageService', 'service:storage');
+  application.inject('service:session', 'storageService', 'storagekit/service:storage');
+  application.inject('adapter', 'storageService', 'storagekit/service:storage');
+  application.inject('controller', 'storageService', 'storagekit/service:storage');
+  application.inject('model', 'storageService', 'storagekit/service:storage');
+  application.inject('route', 'storageService', 'storagekit/service:storage');
 }
 
 export default {
