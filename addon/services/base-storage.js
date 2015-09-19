14968714b15944a8e192e6c46248c815701ecde7
Kenneth Buck <kenneth@nestedsphere.com>
2015-09-19 17:05:57 -0400

    docs

--- addon/services/base-storage.js
+++ addon/services/base-storage.js
@@ -1,25 +1,60 @@
 import Ember from 'ember';
 
+/**
+ * @module ember-cli-storagekit
+ * @submodule adapters
+ */
+
+/**
+ * @class BaseStorage
+ * @namespace EmberCliStoragekit
+ * @extends Ember.Service
+ * @public
+ */
+
 export default Ember.Service.extend({
 
+  /**
+   * @property {*} adapter
+   */
   adapter: null,
 
+  /**
+   * @method setItem
+   * @public
+   */
   setItem(key, value) {
     this.get('adapter').setItem(key, value);
   },
 
+  /**
+   * @method getItem
+   * @public
+   */
   getItem(key) {
     return this.get('adapter').getItem(key);
   },
 
+  /**
+   * @method removeItem
+   * @public
+   */
   removeItem(key) {
     this.get('adapter').removeItem(key);
   },
 
+  /**
+   * @method clear
+   * @public
+   */
   clear() {
     this.get('adapter').clear();
   },
 
+  /**
+   * @method length
+   * @public
+   */
   length() {
     return this.get('adapter').length();
   }
