6628878a71896e1a078d573b327357460e4be5ad
Kenneth Buck <kenneth@nestedsphere.com>
2015-09-19 17:07:40 -0400

    docs

--- addon/utilities/storage-support.js
+++ addon/utilities/storage-support.js
@@ -2,10 +2,30 @@ import Ember from 'ember';
 
 /*global window*/
 
+/**
+ * @module ember-cli-storagekit
+ * @submodule utilities
+ */
+
+/**
+ * @class StorageSupportUtility
+ * @namespace EmberCliStoragekit
+ * @extends Ember.Namespace
+ * @public
+ */
+
 export default Ember.Namespace.create({
 
+  /**
+   * @property {*} global
+   * @private
+   */
   global: window,
 
+  /**
+   * @method has
+   * @public
+   */
   has(type) {
     const _global = this.get('global');
     try {
