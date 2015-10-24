86eec8760d841e22df2c4c83e5a04f7591cd40c3
Kenneth Buck <kenneth@nestedsphere.com>
2015-10-24 15:57:00 -0400

    changed randomMD5 hash

--- addon/utilities/storage-support.js
+++ addon/utilities/storage-support.js
@@ -34,7 +34,7 @@ export default Ember.Namespace.create({
     const _global = this.get('global');
     try {
       // credit: https://gist.github.com/paulirish/5558557
-      const randomMD5 = '11b346184d30d120750d1485e0393812';
+      const randomMD5 = '946d4feb0eac1298f72834168f4dffb2';
 
       _global[type].setItem(randomMD5, randomMD5);
       _global[type].getItem(randomMD5);
